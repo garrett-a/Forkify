7cca6d5524e8ffb5d963d489b3d59559f064518a
garrett-a <94200645+garrett-a@users.noreply.github.com>
2022-02-08 15:26:17 -0600

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '6da86ec7-e610-4491-b125-53c394bb80e1';
 export const MODAL_CLOSE_SEC = 2.5;
